02da552bd44a6159323a0440f2e4e36da350984f
Matthias <matthias.goergens@gmail.com>
2009-09-09 13:56:11 +0200

    removed some stuff

--- facetten/auto.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-name = 'simple'
-
-for n in range(1,13):
-	s = ("../dreiecksmatrix/repre.py %(n)s | ./lrs > exts/%(name)s%(n)s.ext" % {'n': n, 'name':name})
-	print s
-	system(s)
-
